cc14c3c12b958c440aad262a2ab1d2cf97c8a1d8
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2024-01-18 15:24:20 -0300

    Delete AA-StopVmByTAG.ps1

--- AA-StopVmByTAG.ps1
+++
@@ -1,9 +0,0 @@
-# Connect AA
-$azConn = Get-AutomationConnection -Name 'AzureRunAsConnection'
-Add-AzureRMAccount -ServicePrincipal -Tenant $azConn.TenantID -ApplicationId $azConn.ApplicationId -CertificateThumbprint $azConn.CertificateThumbprint
-
-# Stop VM by Tag name - Stop 18:00
-$azVMs = Get-AzureRMVM | Where-Object {$_.Tags.Stop -eq '18:00'}
-
-# Runbook
-$azVMS | Start-AzureRMVM
