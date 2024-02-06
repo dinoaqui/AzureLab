245bec731df7ae12217943629587ec3cdf3db216
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2024-02-06 11:30:08 -0300

    Update Get-AzNetworkInterface.ps1

--- Get-AzNetworkInterface.ps1
+++ Get-AzNetworkInterface.ps1
@@ -6,10 +6,12 @@
 # TenantID
 # SubscriptionID
 
-# Install Excel Modules
+
+# Install and Import the ImportExcel module for Excel operations
 Install-Module -Name ImportExcel -RequiredVersion 7.5.3 -Force -Verbose 
 Import-Module ImportExcel 
 
+# Define a class for network interface card (NIC) properties
 class nics {
     [string]$NicName
     [string]$VMNAME
@@ -19,19 +21,24 @@ class nics {
     [string]$DNS
 }
 
+# Initialize a list to hold NIC objects
 $nicsList = New-Object Collections.Generic.List[nics]
+
+# Retrieve all NIC IDs in the current Azure subscription
 $nicid = (Get-AzNetworkInterface).id
 
-foreach ( $nicsid in $nicid){
+# Iterate over each NIC ID to collect detailed information
+foreach ($nicsid in $nicid){
     $nics = [nics]::new()
-    $nics.NicName = (Get-AzNetworkInterface  -ResourceId $nicsid).Name
-    $nics.VMNAME = (Get-AzNetworkInterface  -ResourceId $nicsid).VirtualMachine.id
-    $nics.ResourceGroupName = (Get-AzNetworkInterface  -ResourceId $nicsid).ResourceGroupName
-    $nics.Location = (Get-AzNetworkInterface  -ResourceId $nicsid).Location
-    $nics.IpConfigurations = (Get-AzNetworkInterface  -ResourceId $nicsid).IpConfigurations.PrivateIpAddress
+    $nics.NicName = (Get-AzNetworkInterface -ResourceId $nicsid).Name
+    $nics.VMNAME = (Get-AzNetworkInterface -ResourceId $nicsid).VirtualMachine.id
+    $nics.ResourceGroupName = (Get-AzNetworkInterface -ResourceId $nicsid).ResourceGroupName
+    $nics.Location = (Get-AzNetworkInterface -ResourceId $nicsid).Location
+    $nics.IpConfigurations = (Get-AzNetworkInterface -ResourceId $nicsid).IpConfigurations.PrivateIpAddress
     $nics.DNS = (Get-AzNetworkInterface -ResourceId $nicsid).DnsSettings.DnsServers
     $nicsList.add($nics)
 }
-# Export Excel and CSV
+
+# Export the collected NIC information to an Excel file and a CSV file
 $nicsList | Export-Excel -Path .\nics.xlsx -WorksheetName 'NICS' -AutoSize -TableStyle 'Light9' -FreezeTopRow -Append
-$nicsList | Export-Csv nics.csv
+$nicsList | Export-Csv -Path nics.csv -NoTypeInformation
