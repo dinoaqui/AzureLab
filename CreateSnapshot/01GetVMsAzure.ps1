9e69ed264f083b0da6784e23a955f008666c5b8b
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2024-06-03 17:47:17 -0300

    Update 01GetVMsAzure.ps1

--- CreateSnapshot/01GetVMsAzure.ps1
+++ CreateSnapshot/01GetVMsAzure.ps1
@@ -1,34 +1,36 @@
-# Parâmetro de entrada: Nome do grupo de recursos
+# Parameter input: Name of the resource group
+# Usage: 01GetVMsAzure.ps1 -ResourceGroupName "ResourceGroupName"
+
 param (
     [string]$ResourceGroupName
 )
 
-# Autenticar no Azure
+# Authenticate to Azure
 Connect-AzAccount
 
-# Coletar VMs no grupo de recursos especificado
+# Collect VMs in the specified resource group
 $vms = Get-AzVM -ResourceGroupName $ResourceGroupName
 
-# Criar uma lista para armazenar as informações das VMs e discos
+# Create a list to store the information of VMs and disks
 $vmList = @()
 
 foreach ($vm in $vms) {
-    # Obter informações do disco de sistema operacional
+    # Get information of the OS disk
     $osDisk = $vm.StorageProfile.OsDisk
 
-    # Obter informações dos discos de dados
+    # Get information of the data disks
     $dataDisks = $vm.StorageProfile.DataDisks
 
-    # Criar um objeto com as informações da VM e discos
+    # Create an object with the information of the VM and disks
     $vmInfo = [PSCustomObject]@{
         VMName     = $vm.Name
         OSDiskName = $osDisk.Name
         DataDisks  = $dataDisks.Name -join ", "
     }
 
-    # Adicionar o objeto à lista
+    # Add the object to the list
     $vmList += $vmInfo
 }
 
-# Salvar a lista em um arquivo CSV
+# Save the list to a CSV file
 $vmList | Export-Csv -Path "./VMsAndDisks.csv" -NoTypeInformation
