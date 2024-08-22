1f7ab730e130410aae6c5526416ff8bb774bb2df
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2024-08-22 18:42:01 -0300

    Update MoveDiskByRegion.ps1

--- MoveDiskByRegion.ps1
+++ MoveDiskByRegion.ps1
@@ -1,39 +1,53 @@
 # Optional
 # Set-AzContext -Subscription "xxxx-xxxx-xxxx-xxxx"
 
-# Name of the Managed Disk you are starting with
-$sourceDiskName = "disk_name_source-osdisk-20200720-012539"
-# Name of the resource group the source disk resides in
-$sourceRG = "RG-ProductionResources"
-# Name you want the destination disk to have
-$targetDiskName = "disk_name_destination-osdisk-20200720-0125399"
-# Name of the resource group to create the destination disk in
-$targetRG = "RG_Destination"
-# Azure region the target disk will be in
-$targetLocate = "EastUS"
- 
+# Name of the managed disk you are starting with
+$sourceDiskName = "DiskName_DataDisk_0" # This disk is located in the Brazil Southeast region
+
+# Name of the resource group where the source disk resides
+$sourceRG = "RG_ResourceGroupName"
+
+# Name you want the destination disk to have - Destination region is Brazil South
+$targetDiskName = "DiskName_DataDisk_0_BrazilSouth"
+
+# Name of the resource group where the destination disk will be created
+$targetRG = "RG_ResourceGroupName"
+
+# Azure region where the destination disk will be created
+$targetLocate = "brazilsouth"
+
 # Gather properties of the source disk
 $sourceDisk = Get-AzDisk -ResourceGroupName $sourceRG -DiskName $sourceDiskName
- 
-# Create the target disk config, adding the sizeInBytes with the 512 offset, and the -Upload flag
+
+# Configure the destination disk, adding the size with the 512 offset, and the -Upload flag
 # If this is an OS disk, add this property: -OsType $sourceDisk.OsType
-# $targetDiskconfig = New-AzDiskConfig -SkuName 'Standard_LRS' -OsType $sourceDisk.OsType -UploadSizeInBytes $($sourceDisk.DiskSizeBytes+512) -Location $targetLocate -CreateOption 'Upload'
-$targetDiskconfig = New-AzDiskConfig -SkuName 'Standard_LRS' -OsType $sourceDisk.OsType -UploadSizeInBytes $($sourceDisk.DiskSizeBytes+512) -Location $targetLocate -CreateOption 'Upload'
- 
-# Create the target disk (empty)
-$targetDisk = New-AzDisk -ResourceGroupName $targetRG -DiskName $targetDiskName -Disk $targetDiskconfig
- 
+# Additionally, add the VM generation and security type if necessary
+#   -SkuName 'Standard_LRS' `
+$targetDiskConfig = New-AzDiskConfig `
+    -SkuName 'Premium_LRS' `  # Configure the disk to use Premium SSD LRS
+    -Location $targetLocate `
+    -CreateOption 'Upload' `
+    -UploadSizeInBytes $($sourceDisk.DiskSizeBytes + 512) `  # Add 512 bytes for alignment
+    -OsType $sourceDisk.OsType `  # Specify the OS type if it's an OS disk
+    -HyperVGeneration $sourceDisk.HyperVGeneration  # Preserve the VM generation
+
+# Add the security profile if it exists on the source disk
+if ($sourceDisk.SecurityProfile) {
+    $targetDiskConfig.SecurityProfile = $sourceDisk.SecurityProfile
+}
+
+# Create the destination disk (empty)
+$targetDisk = New-AzDisk -ResourceGroupName $targetRG -DiskName $targetDiskName -Disk $targetDiskConfig
+
 # Get a SAS token for the source disk, so that AzCopy can read it
 $sourceDiskSas = Grant-AzDiskAccess -ResourceGroupName $sourceRG -DiskName $sourceDiskName -DurationInSecond 86400 -Access 'Read'
- 
-# Get a SAS token for the target disk, so that AzCopy can write to it
+
+# Get a SAS token for the destination disk, so that AzCopy can write to it
 $targetDiskSas = Grant-AzDiskAccess -ResourceGroupName $targetRG -DiskName $targetDiskName -DurationInSecond 86400 -Access 'Write'
- 
-# Begin the copy!
+
+# Start the copy using AzCopy
 azcopy copy $sourceDiskSas.AccessSAS $targetDiskSas.AccessSAS --blob-type PageBlob
- 
+
 # Revoke the SAS so that the disk can be used by a VM
 Revoke-AzDiskAccess -ResourceGroupName $sourceRG -DiskName $sourceDiskName
- 
-# Revoke the SAS so that the disk can be used by a VM
 Revoke-AzDiskAccess -ResourceGroupName $targetRG -DiskName $targetDiskName
