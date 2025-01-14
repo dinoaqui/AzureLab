cc432283844e8c7cb56d0c6dcb5b1f97e463587a
Diego Aquino <79656621+dinoaqui@users.noreply.github.com>
2025-01-14 12:27:47 -0300

    Update MoveDiskByRegion.ps1

--- MoveDiskByRegion.ps1
+++ MoveDiskByRegion.ps1
@@ -1,53 +1,53 @@
 # Optional
-# Set-AzContext -Subscription "xxxx-xxxx-xxxx-xxxx"
+# Set-AzContext -Subscription "XXXXXXXXXXXXXXXXXXXXXX"
 
 # Name of the managed disk you are starting with
-$sourceDiskName = "DiskName_DataDisk_0" # This disk is located in the Brazil Southeast region
+$sourceDiskName1 = "DiskName_Source" # This disk is located in the Brazil Southeast region
 
 # Name of the resource group where the source disk resides
-$sourceRG = "RG_ResourceGroupName"
+$sourceRG1 = "RG_NameSource"
 
 # Name you want the destination disk to have - Destination region is Brazil South
-$targetDiskName = "DiskName_DataDisk_0_BrazilSouth"
+$targetDiskName1 = "DiskName_Destination"
 
 # Name of the resource group where the destination disk will be created
-$targetRG = "RG_ResourceGroupName"
+$targetRG1 = "RG_NameDestination"
 
 # Azure region where the destination disk will be created
-$targetLocate = "brazilsouth"
+$targetLocate1 = "DestinationRegion"
 
 # Gather properties of the source disk
-$sourceDisk = Get-AzDisk -ResourceGroupName $sourceRG -DiskName $sourceDiskName
+$sourceDisk1 = Get-AzDisk -ResourceGroupName $sourceRG1 -DiskName $sourceDiskName1
 
 # Configure the destination disk, adding the size with the 512 offset, and the -Upload flag
 # If this is an OS disk, add this property: -OsType $sourceDisk.OsType
 # Additionally, add the VM generation and security type if necessary
 #   -SkuName 'Standard_LRS' `
-$targetDiskConfig = New-AzDiskConfig `
-    -SkuName 'Premium_LRS' `  # Configure the disk to use Premium SSD LRS
-    -Location $targetLocate `
+$targetDiskConfig1 = New-AzDiskConfig `
+    -SkuName 'Standard_LRS' `
+    -Location $targetLocate1 `
     -CreateOption 'Upload' `
-    -UploadSizeInBytes $($sourceDisk.DiskSizeBytes + 512) `  # Add 512 bytes for alignment
-    -OsType $sourceDisk.OsType `  # Specify the OS type if it's an OS disk
-    -HyperVGeneration $sourceDisk.HyperVGeneration  # Preserve the VM generation
+    -UploadSizeInBytes ($sourceDisk1.DiskSizeBytes + 512) `
+    -OsType $sourceDisk1.OsType `
+    -HyperVGeneration $sourceDisk1.HyperVGeneration
 
 # Add the security profile if it exists on the source disk
-if ($sourceDisk.SecurityProfile) {
-    $targetDiskConfig.SecurityProfile = $sourceDisk.SecurityProfile
+if ($sourceDisk1.SecurityProfile) {
+    $targetDiskConfig1.SecurityProfile = $sourceDisk1.SecurityProfile
 }
 
 # Create the destination disk (empty)
-$targetDisk = New-AzDisk -ResourceGroupName $targetRG -DiskName $targetDiskName -Disk $targetDiskConfig
+$targetDisk1 = New-AzDisk -ResourceGroupName $targetRG1 -DiskName $targetDiskName1 -Disk $targetDiskConfig1
 
 # Get a SAS token for the source disk, so that AzCopy can read it
-$sourceDiskSas = Grant-AzDiskAccess -ResourceGroupName $sourceRG -DiskName $sourceDiskName -DurationInSecond 86400 -Access 'Read'
+$sourceDiskSas1 = Grant-AzDiskAccess -ResourceGroupName $sourceRG1 -DiskName $sourceDiskName1 -DurationInSecond 86400 -Access 'Read'
 
 # Get a SAS token for the destination disk, so that AzCopy can write to it
-$targetDiskSas = Grant-AzDiskAccess -ResourceGroupName $targetRG -DiskName $targetDiskName -DurationInSecond 86400 -Access 'Write'
+$targetDiskSas1 = Grant-AzDiskAccess -ResourceGroupName $targetRG1 -DiskName $targetDiskName1 -DurationInSecond 86400 -Access 'Write'
 
 # Start the copy using AzCopy
-azcopy copy $sourceDiskSas.AccessSAS $targetDiskSas.AccessSAS --blob-type PageBlob
+./azcopy.exe copy $sourceDiskSas1.AccessSAS $targetDiskSas1.AccessSAS --blob-type PageBlob
 
 # Revoke the SAS so that the disk can be used by a VM
-Revoke-AzDiskAccess -ResourceGroupName $sourceRG -DiskName $sourceDiskName
-Revoke-AzDiskAccess -ResourceGroupName $targetRG -DiskName $targetDiskName
+Revoke-AzDiskAccess -ResourceGroupName $sourceRG1 -DiskName $sourceDiskName1
+Revoke-AzDiskAccess -ResourceGroupName $targetRG1 -DiskName $targetDiskName1
